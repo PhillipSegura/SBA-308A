44fe4111b690035536f7dce17169a74eb6929018
PhillipSegura <PhillipaSegura@gmail.com>
2024-11-26 01:10:13 -0700

    completed Async function and now going to create frame html simple frame work to search and see the pokemon sprit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Starting over but this time with the easy yet reliable pokeapi
 // First and for most starting with a Fetch function
 
-fetch("https://pokeapi.co/api/v2/pokemon/Spongebob)")
+/* fetch("https://pokeapi.co/api/v2/pokemon/Spongebob)")
   .then((response) => {
     if (!response.ok) {
       throw new Error("Could not fetchc resource");
@@ -11,3 +11,23 @@ fetch("https://pokeapi.co/api/v2/pokemon/Spongebob)")
   })
   .then((data) => console.log(data))
   .catch((error) => console.error(error));
+  */
+
+// Now let's use async/await for better readability and error handling
+
+fetchData();
+
+async function fetchData() {
+  try {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/Pikachu");
+
+    if (!response.ok) {
+      throw new Error("Could not fetch resource");
+    }
+
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
